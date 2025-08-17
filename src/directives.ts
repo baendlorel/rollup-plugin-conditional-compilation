c84c5e366c021e2cad928bde9ec75f34361b13be
aldia_mechdream <futami16237@gmail.com>
2025-08-17 15:30:09 +0800

    chore: 修改包名，创建基本内容

---
+++ src/directives.ts
@@ -0,0 +1,10 @@
+export const enum Dirv {
+  PREFIX = '/**',
+  SUFFIX = '*/',
+
+  // Basic directives, act like they are in C++
+  IF = '#if',
+  ELSE = '#else',
+  ELIF = '#elif',
+  ENDIF = '#endif',
+}
